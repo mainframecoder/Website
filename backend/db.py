41af840ddf5e8bae7fb7dc16a1a973ebc792469b
mainframecoder <88354899+mainframecoder@users.noreply.github.com>
2026-04-10 14:52:09 -0600

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -4,10 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URL = os.getenv("MONGO_URL")
-
-client = MongoClient(MONGO_URL)
+client = MongoClient(os.getenv("MONGO_URL"))
 db = client["ecommerce"]
 
-products_collection = db["products"]
 orders_collection = db["orders"]
