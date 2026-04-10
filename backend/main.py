a37e3d2299134804021564e69bf1462b45bba370
mainframecoder <88354899+mainframecoder@users.noreply.github.com>
2026-04-10 17:23:00 -0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-from routes import products, orders, payment
+from routes import payment
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(products.router, prefix="/products")
-app.include_router(orders.router, prefix="/orders")
 app.include_router(payment.router, prefix="/payment")
 
 @app.get("/")
 def home():
-    return {"msg": "API running"}
+    return {"msg": "Backend running"}
