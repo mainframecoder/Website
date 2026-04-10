7039f63f03087d346275fb9bdd1b97bcb65cc4d2
mainframecoder <88354899+mainframecoder@users.noreply.github.com>
2026-04-10 14:52:42 -0600

    Update products.py

--- backend/routes/products.py
+++ backend/routes/products.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter
-from db import products_collection
 
 router = APIRouter()
 
 @router.get("/")
 def get_products():
-    products = list(products_collection.find({}, {"_id": 0}))
-    return products
+    return {"msg": "products endpoint"}
