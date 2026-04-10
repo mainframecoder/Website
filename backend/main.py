d924fbf8d9da1512e48f769fec973337219fafbf
mainframecoder <88354899+mainframecoder@users.noreply.github.com>
2026-04-10 05:18:17 -0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # keep * for now
     allow_methods=["*"],
     allow_headers=["*"],
 )
