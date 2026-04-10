ffda91a5cfda11a22df463570d51bcb7fedd73c3
mainframecoder <88354899+mainframecoder@users.noreply.github.com>
2026-04-10 13:47:08 -0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -5,23 +5,20 @@ from routes import products, orders, payment
 
 app = FastAPI()
 
-# ✅ CORS FIX (VERY IMPORTANT)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://website-pxrk46uki-mainframecoders-projects.vercel.app"
+        "https://website-qpqdr5hjm-mainframecoders-projects.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# ✅ ROUTES
 app.include_router(products.router, prefix="/products")
 app.include_router(orders.router, prefix="/orders")
 app.include_router(payment.router, prefix="/payment")
 
-# ✅ HEALTH CHECK
 @app.get("/")
 def home():
     return {"msg": "API running"}
