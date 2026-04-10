0a9a5e6e21ca4e57c15ded82c9a04e14430e61cc
mainframecoder <88354899+mainframecoder@users.noreply.github.com>
2026-04-10 05:17:20 -0600

    Update payment.py

--- backend/routes/payment.py
+++ backend/routes/payment.py
@@ -1,25 +1,47 @@
 import os
 import stripe
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter()
 
 stripe.api_key = os.getenv("STRIPE_SECRET")
+FRONTEND_URL = os.getenv("FRONTEND_URL")
+
+
+class CartItem(BaseModel):
+    name: str
+    price: float
+    qty: int
+
+
+class CartRequest(BaseModel):
+    items: list[CartItem]
+
 
 @router.post("/create-checkout-session")
-def create_checkout():
+def create_checkout(data: CartRequest):
+
+    line_items = []
+
+    for item in data.items:
+        line_items.append({
+            "price_data": {
+                "currency": "usd",   # change to "inr" if needed
+                "product_data": {
+                    "name": item.name
+                },
+                "unit_amount": int(item.price * 100),
+            },
+            "quantity": item.qty,
+        })
+
     session = stripe.checkout.Session.create(
         payment_method_types=["card"],
-        line_items=[{
-            "price_data": {
-                "currency": "usd",
-                "product_data": {"name": "Test Product"},
-                "unit_amount": 5000,
-            },
-            "quantity": 1,
-        }],
+        line_items=line_items,
         mode="payment",
-        success_url="http://localhost:5500/success.html",
-        cancel_url="http://localhost:5500",
+        success_url=f"{FRONTEND_URL}/success.html",
+        cancel_url=f"{FRONTEND_URL}",
     )
+
     return {"url": session.url}
