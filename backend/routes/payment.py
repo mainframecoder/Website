398001803447c72acac02e72607403a5a486d05f
mainframecoder <88354899+mainframecoder@users.noreply.github.com>
2026-04-11 11:02:16 -0600

    Update payment.py

--- backend/routes/payment.py
+++ backend/routes/payment.py
@@ -1,36 +1,45 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter
 import stripe
 import os
 
-router = APIRouter(prefix="/payment")
+from models import CheckoutRequest
+from db import PRODUCTS
+
+router = APIRouter()
 
 stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
-
 FRONTEND_URL = os.getenv("FRONTEND_URL")
 
+
 @router.post("/create-checkout-session")
-async def create_checkout_session(request: Request):
-    data = await request.json()
-    cart = data.get("cart", [])
+def create_checkout(data: CheckoutRequest):
+
+    if not stripe.api_key:
+        return {"error": "Stripe key missing"}
 
     line_items = []
 
-    for item in cart:
+    for item in data.cart:
+        product = PRODUCTS.get(item.id)
+        if not product:
+            continue
+
         line_items.append({
             "price_data": {
                 "currency": "cad",
                 "product_data": {
-                    "name": item["name"],
+                    "name": product["name"],
                 },
-                "unit_amount": int(item["price"] * 100),
+                "unit_amount": int(product["price"] * 100),
             },
-            "quantity": item["qty"],
+            "quantity": item.qty,
         })
 
     session = stripe.checkout.Session.create(
         payment_method_types=["card"],
         line_items=line_items,
         mode="payment",
+        customer_email=data.email,
         success_url=f"{FRONTEND_URL}/success.html",
         cancel_url=f"{FRONTEND_URL}",
     )
