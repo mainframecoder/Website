284a1ea5df5fb0b169c13d79e9429604cb46b2d1
mainframecoder <88354899+mainframecoder@users.noreply.github.com>
2026-04-11 11:01:55 -0600

    Update orders.py

--- backend/routes/orders.py
+++ backend/routes/orders.py
@@ -1,18 +1,14 @@
 from fastapi import APIRouter
-from db import orders_collection
-from bson import ObjectId
 
 router = APIRouter()
 
-@router.get("/{order_id}")
-def track_order(order_id: str):
-    order = orders_collection.find_one({"_id": ObjectId(order_id)})
+ORDERS = []
 
-    if not order:
-        return {"error": "Order not found"}
+@router.post("/")
+def create_order(order: dict):
+    ORDERS.append(order)
+    return {"msg": "Order stored"}
 
-    return {
-        "status": order["status"],
-        "name": order["name"],
-        "address": order["address"]
-    }
+@router.get("/")
+def get_orders():
+    return ORDERS
