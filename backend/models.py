1d3d3fa3bedd6230e9bb216a6bccd879bdbb8869
mainframecoder <88354899+mainframecoder@users.noreply.github.com>
2026-04-14 05:20:59 -0600

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,17 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
-from db import Base
+from database import Base
+
+class Store(Base):
+    __tablename__ = "stores"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    domain = Column(String)
+
+    users = relationship("User", back_populates="store")
+    products = relationship("Product", back_populates="store")
+
 
 class User(Base):
     __tablename__ = "users"
@@ -8,6 +19,9 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String, unique=True)
     password = Column(String)
+    store_id = Column(Integer, ForeignKey("stores.id"))
+
+    store = relationship("Store", back_populates="users")
 
 
 class Product(Base):
@@ -16,7 +30,9 @@ class Product(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     description = Column(String)
+    store_id = Column(Integer, ForeignKey("stores.id"))
 
+    store = relationship("Store", back_populates="products")
     variants = relationship("Variant", back_populates="product")
 
 
@@ -39,5 +55,4 @@ class Order(Base):
 
     id = Column(Integer, primary_key=True)
     email = Column(String)
-    status = Column(String)
     total = Column(Float)
