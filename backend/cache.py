2e7208f60c81bea354f145d6fd2e69809256fc52
mainframecoder <88354899+mainframecoder@users.noreply.github.com>
2026-04-14 07:38:53 -0600

    Update cache.py

--- backend/cache.py
+++ backend/cache.py
@@ -1,4 +1,11 @@
-import redis
 import os
 
-redis_client = redis.from_url(os.getenv("REDIS_URL"))
+redis_client = None
+
+try:
+    import redis
+    url = os.getenv("REDIS_URL")
+    if url:
+        redis_client = redis.from_url(url)
+except:
+    redis_client = None
