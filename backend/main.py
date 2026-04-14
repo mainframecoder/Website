dcf484ecc9b7978a6abf963630c66c88dd97e29e
mainframecoder <88354899+mainframecoder@users.noreply.github.com>
2026-04-14 07:39:44 -0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,16 @@
 from fastapi import FastAPI
-from fastapi.middleware.gzip import GZipMiddleware
-
-from routes import products, auth, payment, orders
+from database import Base, engine
+from routes import products, auth, orders, payment
 
 app = FastAPI()
 
-app.add_middleware(GZipMiddleware, minimum_size=1000)
+# create tables
+Base.metadata.create_all(bind=engine)
 
 app.include_router(products.router, prefix="/products")
 app.include_router(auth.router, prefix="/auth")
-app.include_router(payment.router, prefix="/payment")
 app.include_router(orders.router, prefix="/orders")
+app.include_router(payment.router, prefix="/payment")
 
 @app.get("/")
 def root():
