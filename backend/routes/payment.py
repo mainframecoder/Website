b90d52d8705242bc0e297da920f036a0de31085e
mainframecoder <88354899+mainframecoder@users.noreply.github.com>
2026-04-14 07:45:04 -0600

    Update payment.py

--- backend/routes/payment.py
+++ backend/routes/payment.py
@@ -1,10 +1,30 @@
-import stripe
 from fastapi import APIRouter
-
-stripe.api_key = "sk_test"
+from database import SessionLocal
+from models import Order
+import json
 
 router = APIRouter()
 
+
 @router.post("/create-checkout-session")
-def checkout():
-    return {"url": "https://checkout.stripe.com"}
+def checkout(data: dict):
+    db = SessionLocal()
+
+    items = data.get("items", [])
+    email = data.get("email")
+
+    total = sum([i["qty"] * 50 for i in items])
+
+    order = Order(
+        email=email,
+        total=total,
+        items=json.dumps(items)
+    )
+
+    db.add(order)
+    db.commit()
+    db.refresh(order)
+
+    return {
+        "url": f"/success.html?order_id={order.id}"
+    }
