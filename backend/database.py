a42484cba2928e471132ea008f7e4f7ebe797722
mainframecoder <88354899+mainframecoder@users.noreply.github.com>
2026-04-14 07:37:54 -0600

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -4,12 +4,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True,
-    pool_size=10,
-    max_overflow=20
-)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(bind=engine)
 
